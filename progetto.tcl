666a171cd51801b14128606a63e1ad7f17aa7dda
steno97 <46044807+steno97@users.noreply.github.com>
2021-06-13 16:53:16 +0200

    Update progetto.tcl

--- progetto.tcl
+++ progetto.tcl
@@ -5,7 +5,8 @@ global area_iniziale, leakage_power, switching_power
 
 #################################################
 proc evaluate_area { } {
-	#stefano
+	set area [get_attribute [get_design] area ]
+	return $area
 }
 
 ##################################################
@@ -21,18 +22,18 @@ proc leakage_power { } {
 }
 
 ####################################################
-proc swap { cell_list vt_type} {
+proc cells_swapping { cell_list vt_type} {
 	set lista [list]
 	foreach cell $cell_list {
 		set lib_cell [get_lib_cell -of_object $cell]
 		set type [get_attribute $lib_cell threshold_voltage_group]
-		#puts $type
+		puts $type
 		if { $type!=$vt_type} {
 			set collection [get_alternative_lib_cells $cell]
 			foreach_in_collection t $collection {
 				#set lib_cell [get_lib_cell -of_object $cell]
 				set type [get_attribute $t threshold_voltage_group]
-				#puts $type
+				puts $type
 				if {$type ==$vt_type} {
 					size_cell $cell $t
 					lappend lista $t
@@ -43,7 +44,7 @@ proc swap { cell_list vt_type} {
 	}
 	return $lista
 }
-}
+
 
 ######################################################
 proc score { } {
