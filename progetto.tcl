08a300108ff21a8e00e37adf5755804abd309557
steno97 <46044807+steno97@users.noreply.github.com>
2021-06-13 09:46:53 +0200

    Update progetto.tcl

--- progetto.tcl
+++ progetto.tcl
@@ -21,8 +21,28 @@ proc leakage_power { } {
 }
 
 ####################################################
-proc swap { } {
-	#stefano
+proc swap { cell_list vt_type} {
+	set lista [list]
+	foreach cell $cell_list {
+		set lib_cell [get_lib_cell -of_object $cell]
+		set type [get_attribute $lib_cell threshold_voltage_group]
+		#puts $type
+		if { $type!=$vt_type} {
+			set collection [get_alternative_lib_cells $cell]
+			foreach_in_collection t $collection {
+				#set lib_cell [get_lib_cell -of_object $cell]
+				set type [get_attribute $t threshold_voltage_group]
+				#puts $type
+				if {$type ==$vt_type} {
+					size_cell $cell $t
+					lappend lista $t
+					break
+				}
+			}
+		}	
+	}
+	return $lista
+}
 }
 
 ######################################################
